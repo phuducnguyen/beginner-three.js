aa5f0b836053246d453190e3b40205d678166da8
Phu Duc Nguyen <sillycat@duck.com>
2023-04-13 14:40:30 +0700

    Add a box mesh to the scene

--- my-portfolio/main.js
+++ my-portfolio/main.js
@@ -1,6 +1,5 @@
-// Find the latest version by visiting 
-// https://www.npmjs.com/package/three/v/0.151.3
-import * as THREE from 'three';
+// Find the latest version by visiting https://unpkg.com/three
+import * as THREE from 'https://unpkg.com/three@0.151.3/build/three.module.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -12,3 +11,13 @@ document.body.appendChild( renderer.domElement );
 console.log(scene);
 console.log(camera);
 console.log(renderer);
+
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+console.log(geometry);
+console.log(material);
