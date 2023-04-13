c9eee458ff4603ff8b1f647335eae3bbd8a11c62
Phu Duc Nguyen <sillycat@duck.com>
2023-04-13 19:29:57 +0700

    Add depth / jaggedness to plane

--- my-portfolio/main.js
+++ my-portfolio/main.js
@@ -7,18 +7,29 @@ import * as THREE from 'https://unpkg.com/three@0.151.3/build/three.module.js';
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 const renderer = new THREE.WebGLRenderer();
-const geometry = new THREE.BoxGeometry( 2, 2, 2 );
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshBasicMaterial( { color: 0xAABBFF } );
 const cube = new THREE.Mesh( geometry, material );
 
 const planeGeometry = new THREE.PlaneGeometry( 5, 5, 10, 10 );
 const planeMaterial = new THREE.MeshPhongMaterial({
-  color: 0xff0000,
-  side: THREE.DoubleSide
+  color: 0xAABBCC,
+  side: THREE.DoubleSide,
+  flatShading: true
 })
 const plane = new THREE.Mesh( planeGeometry, planeMaterial );
 
-const light = new THREE.DirectionalLight(0xffffff, 1);
+const light = new THREE.DirectionalLight(0xAABBCC, 1);
+
+const array = plane.geometry.attributes.position.array;
+
+for (let i = 0; i < array.length; i += 3) {
+  const x = array[i];
+  const y = array[i + 1];
+  const z = array[i + 2];
+  array[i + 2] = z + Math.random();
+  console.log({x, y, z});
+} 
 
 // Renderer config
 renderer.setPixelRatio(devicePixelRatio);
@@ -27,7 +38,6 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 light.position.set( 0, 0, 1 )
 
-scene.add( cube );
 scene.add( plane );
 scene.add( light );
 
@@ -39,7 +49,7 @@ camera.position.z = 5;
 // console.log(scene);
 // console.log(camera);
 // console.log(renderer);
-console.log(plane.geometry.attributes.position.array);
+// console.log(plane.geometry.attributes.position.array);
 
 /* Utility Function */
 
@@ -49,9 +59,7 @@ function animate() {
   requestAnimationFrame(animate);
   renderer.render( scene, camera );
 
-  // Animation the cube
-  cube.rotation.x += 0.01
-  cube.rotation.y += 0.01
+  // Animation the mesh
   plane.rotation.x += 0.01
 }
 
